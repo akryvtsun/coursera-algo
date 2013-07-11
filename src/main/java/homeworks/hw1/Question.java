68ed871f34227d08036b25c4ecde7a9e6e48e692
Andriy Kryvtsun <Andriy_Kryvtsun@epam.com>
2013-07-11 15:59:24 +0300

    Adding some simple test cases

--- src/main/java/homeworks/hw1/Question.java
+++ src/main/java/homeworks/hw1/Question.java
@@ -1,6 +1,16 @@
 package homeworks.hw1;
 
+import java.util.List;
+
 public class Question {
+
+    public Question(List<Integer> array) {
+
+    }
+
+    int getAnswer() {
+        return 0;
+    }
 
     public static void main(String... args) {
         System.out.println( "Hello World!" );
--- src/test/java/homeworks/hw1/QuestionTest.java
+++ src/test/java/homeworks/hw1/QuestionTest.java
@@ -2,15 +2,50 @@ package homeworks.hw1;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 /**
- * Unit test for simple Question.
+ * Unit tests Question.
+ *
+ * @see <a href="https://class.coursera.org/algo-004/forum/thread?thread_id=52">Test Cases for Programming Assignment #1</a>
  */
 public class QuestionTest {
 
     @Test
-    public void testQuestion() {
-        assertTrue(true);
+    public void testQuestion1() {
+        Question q = new Question(Arrays.asList(1,3,5,2,4,6));
+        assertEquals(3, q.getAnswer());
+    }
+
+    @Test
+    public void testQuestion2() {
+        Question q = new Question(Arrays.asList(1,5,3,2,4));
+        assertEquals(4, q.getAnswer());
+    }
+
+    @Test
+    public void testQuestion3() {
+        Question q = new Question(Arrays.asList(5,4,3,2,1));
+        assertEquals(10, q.getAnswer());
+    }
+
+    @Test
+    public void testQuestion4() {
+        Question q = new Question(Arrays.asList(1,6,3,2,4,5));
+        assertEquals(5, q.getAnswer());
+    }
+
+    @Test
+    public void testQuestion5() {
+        Question q = new Question(Arrays.asList(1,2,3,4,5,6));
+        assertEquals(0, q.getAnswer());
+    }
+
+    @Test
+    public void testQuestion6() {
+        Question q = new Question(Arrays.asList(6,5,4,3,2,1));
+        assertEquals(15, q.getAnswer());
     }
 }
