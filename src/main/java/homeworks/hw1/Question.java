bf9c86fdec945fcf4137c3763736654f06ae18bc
Andriy Kryvtsun <Andriy_Kryvtsun@epam.com>
2013-07-11 15:42:19 +0300

    Unit test refactoring

---
+++ src/main/java/homeworks/hw1/Question.java
@@ -0,0 +1,8 @@
+package homeworks.hw1;
+
+public class Question {
+
+    public static void main(String... args) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/homeworks/hw1/QuestionTest.java
@@ -0,0 +1,16 @@
+package homeworks.hw1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple Question.
+ */
+public class QuestionTest {
+
+    @Test
+    public void testQuestion() {
+        assertTrue(true);
+    }
+}
