15c24b885060ebe2b46911e1a6dced4beda45ed4
Andriy Kryvtsun <Andriy_Kryvtsun@epam.com>
2013-07-16 17:17:26 +0300

    Adding new TODOs

--- src/test/java/homeworks/week2/QuestionTest.java
+++ src/test/java/homeworks/week2/QuestionTest.java
@@ -8,10 +8,12 @@ import static org.junit.Assert.assertEquals;
  * todo enter array for partitioning
  * todo user triangulation for test partitioning on another data set
  * todo test partitioning procedure
+ * todo test QuickSort procedure
  *
  * todo test pivot element selection
  * todo test real data loader
  *
+ * todo how to make obtaining of real answer as part of junit unfrastructure?
  * todo make real data loader common for all weeks???
  */
 public class QuestionTest {
